56717ede93555c314eed82d817b9ec717381ed74
sravani1119 <52592618+sravani1119@users.noreply.github.com>
2022-11-29 00:29:26 -0600

    S546550 modified confirmed apmnt activity

--- app/src/main/java/com/ensias/healthcareapp/ConfirmedAppointmensActivity.java
+++ app/src/main/java/com/ensias/healthcareapp/ConfirmedAppointmensActivity.java
@@ -58,6 +58,6 @@ public class ConfirmedAppointmensActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-        adapter.stopListening();
+        
     }
 }
